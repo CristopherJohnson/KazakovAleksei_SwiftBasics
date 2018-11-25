a6efbc250370043cd230d63c579fcc74a8df0834
CristopherJohnson <henkok127@mail.ru>
2018-11-25 12:34:58 +0300

    starting homework

---
+++ L2_KazakovAleksei/L2_KazakovAleksei/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L2_KazakovAleksei
+//
+//  Created by Алексей Казаков on 25/11/2018.
+//  Copyright © 2018 Алексей Казаков. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
