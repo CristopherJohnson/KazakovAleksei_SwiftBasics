cf5f8c8a771372addd9cc43d391ac1aa902f669e
CristopherJohnson <henkok127@mail.ru>
2018-11-25 13:23:29 +0300

    сделал задание 1

--- L2_KazakovAleksei/L2_KazakovAleksei/main.swift
+++ L2_KazakovAleksei/L2_KazakovAleksei/main.swift
@@ -8,5 +8,16 @@
 
 import Foundation
 
-print("Hello, World!")
+//Lesson2
+//Task 1
 
+func evenNumbers(comparingNumber number: Int) -> Bool{
+    if (number % 2) == 0{
+        return true
+    } else {
+        return false
+    }
+}
+
+let testNumber = evenNumbers(comparingNumber: 6)
+print(testNumber)
